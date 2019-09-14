cb228ab6847fc8f0b1da02f4a772cc79735d2b52
huyen <huyennt.cntt@gmail.com>
2019-09-14 07:34:39 +0700

    update jsx

---
+++ 01_JSX/jsx/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+      header
+      </div>
+        <div>
+            Content!
+        </div>
+    <div>
+      footer
+      </div>
+      </div>
+  );
+}
+
+export default App;
