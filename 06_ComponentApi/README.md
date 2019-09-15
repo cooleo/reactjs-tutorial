9504c3e9f1f7d1e57c8ce969f5b3e37865ef1a73
huyen <huyennt.cntt@gmail.com>
2019-09-15 09:28:52 +0700

    update component api

---
+++ 06_ComponentApi/README.md
@@ -0,0 +1,3 @@
+1. setState(): update the state of the component will not method.  This method will not replace the state, but only add changes to the original state.
+2. forceUpdate() : Sometimes we might want to update the component manually. This can be achieved using the forceUpdate() method.
+3. import react-dom if use ReactDOM.findDOMNode() method: change property of DOMNode.
--- 06_ComponentApi/componentapi/src/App.js
+++ 06_ComponentApi/componentapi/src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
 
-export class App extends Component{
+export class App extends React.Component {
   constructor() {
     super();
     this.state = {
       data: []
     }
     this.setStateHandler = this.setStateHandler.bind(this);
+    this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
   };
-  setStateHandler(){
-    var item="Set state..."
-    var myArray= this.state.data.slice();
+
+  setStateHandler() {
+    var item = "Set state..."
+    var myArray = this.state.data.slice();
     myArray.push(item);
 
 
     this.setState({data: myArray})
   };
+findDomNodeHandler() {
+  var myDiv = document.getElementById('myDiv');
+  ReactDOM.findDOMNode(myDiv).style.color = 'green';
+};
   render() {
     return (
         <div>
           <button onClick = {this.setStateHandler}>SET STATE</button>
           <h4>State Array: {this.state.data}</h4>
+          <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
+          <h4>Random number: {Math.random()}</h4>
+          <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
+          <div id = "myDiv">NODE</div>
         </div>
     );
   }
